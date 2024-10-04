e8aa81a080539384b49e1249500a0230867ad9fc
Mikey <132271098+mikeysdigitalassets@users.noreply.github.com>
2024-10-04 12:12:46 -0500

    updates

--- src/main/java/com/example/ecommercebackend/controllers/PaymentController.java
+++ src/main/java/com/example/ecommercebackend/controllers/PaymentController.java
@@ -1,37 +1,39 @@
-package com.example.ecommercebackend.controllers;
+//package com.example.ecommercebackend.controllers;
+//
+//import com.example.ecommercebackend.services.StripeService;
+//import com.stripe.model.PaymentIntent;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import com.example.ecommercebackend.dto.PaymentRequest;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+//@RestController
+//@RequestMapping("/api/payment")
+//public class PaymentController {
+//
+//    @Autowired
+//    private StripeService stripeService;
+//
+//    @PostMapping("/create-payment-intent")
+//    public ResponseEntity<Map<String, String>> createPaymentIntent(@RequestBody PaymentRequest paymentRequest) {
+//        try {
+//            PaymentIntent paymentIntent = stripeService.createPaymentIntent(paymentRequest.getAmount());
+//            Map<String, String> response = new HashMap<>();
+//            response.put("client_secret", paymentIntent.getClientSecret());
+//            return ResponseEntity.ok(response);
+//        } catch (Exception e) {
+//            return ResponseEntity.status(500).body(Map.of("error", "Payment failed: " + e.getMessage()));
+//        }
+//    }
+//
+//
+//
+//
+//
+//}
 
-import com.example.ecommercebackend.services.StripeService;
-import com.stripe.model.PaymentIntent;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import com.example.ecommercebackend.dto.PaymentRequest;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
-@RestController
-@RequestMapping("/api/payment")
-public class PaymentController {
-
-    @Autowired
-    private StripeService stripeService;
-
-    @PostMapping("/create-payment-intent")
-    public ResponseEntity<Map<String, String>> createPaymentIntent(@RequestBody PaymentRequest paymentRequest) {
-        try {
-            PaymentIntent paymentIntent = stripeService.createPaymentIntent(paymentRequest.getAmount());
-            Map<String, String> response = new HashMap<>();
-            response.put("client_secret", paymentIntent.getClientSecret());
-            return ResponseEntity.ok(response);
-        } catch (Exception e) {
-            return ResponseEntity.status(500).body(Map.of("error", "Payment failed: " + e.getMessage()));
-        }
-    }
-
-
-
-
-
-}
+// moved Stripe and order proccessing to GO microservice
--- src/main/java/com/example/ecommercebackend/services/StripeService.java
+++ src/main/java/com/example/ecommercebackend/services/StripeService.java
@@ -1,29 +1,31 @@
-package com.example.ecommercebackend.services;
+//package com.example.ecommercebackend.services;
+//
+//import com.stripe.Stripe;
+//import com.stripe.model.PaymentIntent;
+//import com.stripe.param.PaymentIntentCreateParams;
+//import jakarta.annotation.PostConstruct;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class StripeService {
+//
+//    @Value("${stripe.secret.key}")
+//    private String stripeSecretKey;
+//
+//    @PostConstruct
+//    public void init() {
+//        Stripe.apiKey = stripeSecretKey;
+//    }
+//
+//    public PaymentIntent createPaymentIntent(Long amount) throws Exception {
+//        PaymentIntentCreateParams createParams = PaymentIntentCreateParams.builder()
+//                .setAmount(amount)
+//                .setCurrency("usd")
+//                .build();
+//
+//        return PaymentIntent.create(createParams);
+//    }
+//}
 
-import com.stripe.Stripe;
-import com.stripe.model.PaymentIntent;
-import com.stripe.param.PaymentIntentCreateParams;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
-@Service
-public class StripeService {
-
-    @Value("${stripe.secret.key}")
-    private String stripeSecretKey;
-
-    @PostConstruct
-    public void init() {
-        Stripe.apiKey = stripeSecretKey;
-    }
-
-    public PaymentIntent createPaymentIntent(Long amount) throws Exception {
-        PaymentIntentCreateParams createParams = PaymentIntentCreateParams.builder()
-                .setAmount(amount)
-                .setCurrency("usd")
-                .build();
-
-        return PaymentIntent.create(createParams);
-    }
-}
+// moved stripe & transaction processing to GO microservice
